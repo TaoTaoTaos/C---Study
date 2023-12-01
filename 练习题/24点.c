7ec0a97c82ad515f3d40758d9d08bee026c0ce33
jintao Li <51856377+TaoTaoTaos@users.noreply.github.com>
2023-12-01 14:38:29 +0800

    main

--- 练习题/24点.c
+++ 练习题/24点.c
@@ -1,184 +1,231 @@
-/*
-要求：
-输入1到13的4个整数，看通过加减乘除四种运算，能不能得到24这个结果，如果可以，则输出所有组合情况，而且要用括号表示运算的优先级，如果不行，则输出没有可行的结果。
-
-思路和算法的原理：
-1. 首先，我们需要一个函数来生成所有可能的 运算符 组合。
-2. 然后，我们需要一个函数来生成所有可能的 数字 组合。
-3. 最后，我们需要一个函数来计算每个数字和运算符组合的结果，并检查是否等于24。
-
-*/
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
 
+int n = 4;
+// 定义一个整数n，值为4
+int A[4] = {0};
+// 定义一个长度为4的整数数组A，所有元素初始化为0
+char oper[4] = {'+', '-', '*', '/'};
+// 定义一个长度为4的字符数组oper，存储四种运算符
+char B[4][50];
+// 定义一个二维字符数组B，可以看作是4个长度为50的字符串
+int count = 0;
+// 定义一个整数count，用于计数，初始值为0
 
+/*这个递归函数F的主要目标是找出所有可能的通过加、减、乘、除运算使得四个数字的结果为24的方式。
 
+函数的工作原理如下：
 
-/*
-1.生成所有可能的运算符组合的函数
-我们可以使用递归来生成所有可能的运算符组合。首先，我们创建一个数组来存储运算符，然后我们使用递归函数来添加每个可能的运算符到数组中，直到数组的长度等于数字的数量减一。
-*/
+如果n等于1，那么检查A[0]是否等于24。如果是，那么打印B[0]并增加计数器count。
+对于n个数，两两组合进行四则运算。这是通过两个嵌套的for循环实现的，其中i和j是数组A的索引。
+对于每一对数字a和b（a是A[i]，b是A[j]），执行以下步骤：
+执行加法运算a + b，并递归调用函数F(n - 1)。
+执行减法运算a - b和b - a，并分别递归调用函数F(n - 1)。
+执行乘法运算a * b，并递归调用函数F(n - 1)。
+如果b不为0，执行除法运算a / b，并递归调用函数F(n - 1)。
+如果a不为0，执行除法运算b / a，并递归调用函数F(n - 1)。
+在每次运算后，都会更新A[i]和B[i]的值。A[i]存储运算结果，B[i]存储运算表达式。
+在每次运算前后，都会保存和恢复A[i]、A[j]、B[i]、B[j]的值，以确保在每次递归调用中，数组A和B的其他元素不会被改变。
+这个函数通过递归的方式枚举了所有可能的运算顺序和运算方式，从而找出所有可能的解。最后，函数F(n)会返回到main()函数中，打印出总共有多少种解法。*/
 
-char operators[] = {'+', '-', '*', '/'};// 运算符数组
-
-void generate_operator_combinations(char *combination, int length, int n)
+char answers[100][50]; // 存储所有可能的答案
+int answer_count = 0;  // 存储答案的数量
+// 递归函数F，参数为整数n
+void F(int n)
 {
-    // 如果组合的长度等于n-1，那么就输出这个组合
-    if (length == n - 1)
+    // 如果n为1，检查A[0]是否为24，如果是，打印B[0]并增加计数
+    if (n == 1)
     {
-        combination[length] = '\0';
-        printf("%s\n", combination);
-        return;
+        if (A[0] == 24)
+        {
+            strcpy(answers[answer_count], B[0]); // 将答案存储在answers中
+            answer_count++;                      // 增加答案的数量
+        }
     }
 
-    // 对于每一个可能的运算符，我们都尝试把它添加到组合中，然后递归地生成剩余的组合
+    // 对于n个数，两两组合进行四则运算
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            double a, b;            // 定义两个浮点数a和b
+            char x[50], y[50];      // 定义两个长度为50的字符串x和y
+            a = A[i];               // 将A[i]的值赋给a
+            b = A[j];               // 将A[j]的值赋给b
+            A[j] = A[n - 1];        // 将A[n-1]的值赋给A[j]
+            strcpy(x, B[i]);        // 将B[i]的值复制给x
+            strcpy(y, B[j]);        // 将B[j]的值复制给y
+            strcpy(B[j], B[n - 1]); // 将B[n-1]的值复制给B[j]
+
+            // 对a和b进行加法运算，并递归调用函数F
+            A[i] = a + b;
+            sprintf(B[i], "(%s+%s)", x, y);
+            F(n - 1);
+
+            // 对a和b进行减法运算，并递归调用函数F
+            A[i] = a - b;
+            sprintf(B[i], "(%s-%s)", x, y);
+            F(n - 1);
+            A[i] = b - a;
+            sprintf(B[i], "(%s-%s)", y, x);
+            F(n - 1);
+
+            // 对a和b进行乘法运算，并递归调用函数F
+            A[i] = a * b;
+            sprintf(B[i], "(%s*%s)", x, y);
+            F(n - 1);
+
+            // 如果b不为0，对a和b进行除法运算，并递归调用函数F
+            if (b != 0)
+            {
+                A[i] = a / b;
+                sprintf(B[i], "(%s/%s)", x, y);
+                F(n - 1);
+            }
+            // 如果a不为0，对b和a进行除法运算，并递归调用函数F
+            if (a != 0)
+            {
+                A[i] = b / a;
+                sprintf(B[i], "(%s/%s)", y, x);
+                F(n - 1);
+            }
+
+            // 还原A[i]、A[j]、B[i]、B[j]的值
+            A[i] = a;
+            A[j] = b;
+            strcpy(B[i], x);
+            strcpy(B[j], y);
+        }
+    }
+}
+
+void game1()
+{
+    printf("请输入四个数字（1-13）：\n");
+    for (int i = 0; i < 4; ++i)
+    {
+        scanf("%d", &A[i]); // 读取用户输入的数字并存入数组A
+    }
+    printf("\n");
+    // 将用户输入的数字转换为对应的字符并存入数组B
     for (int i = 0; i < 4; i++)
     {
-        combination[length] = operators[i];
-        generate_operator_combinations(combination, length + 1, n);
-    }
-}
-
-
-
-/*
-2.生成所有可能的数字组合的函数
-我们可以使用递归来生成所有可能的数字组合。首先，我们创建一个数组来存储数字，然后我们使用递归函数来添加每个可能的数字到数组中，直到数组的长度等于4。
-*/
-
-void generate_number_combinations(int *numbers, int *combination, int length, int n)
-{
-    // 如果组合的长度等于n，那么就输出这个组合
-    if (length == n)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            printf("%d ", combination[i]);
-        }
-        printf("\n");
-        return;
-    }
-
-    // 对于每一个可能的数字，我们都尝试把它添加到组合中，然后递归地生成剩余的组合
-    for (int i = 0; i < n; i++)
-    {
-        int is_used = 0;
-        for (int j = 0; j < length; j++)
-        {
-            if (combination[j] == numbers[i])
-            {
-                is_used = 1;
-                break;
-            }
-        }
-        if (!is_used)
-        {
-            combination[length] = numbers[i];
-            generate_number_combinations(numbers, combination, length + 1, n);
-        }
-    }
-}
-
-
-//计算函数，因为我们是通过遍历char数组来获取运算符的，所以要有这个函数来转化
-double calculate(double a, double b, char operator)
-{
-    switch (operator)
-    {
-    case '+':
-        return a + b;
-    case '-':
-        return a - b;
-    case '*':
-        return a * b;
-    case '/':
-        if (b != 0)
-        {
-            return a / b;
-        }
+        if (A[i] == 1)
+            strcpy(B[i], "A");
+        else if (A[i] == 11)
+            strcpy(B[i], "J");
+        else if (A[i] == 12)
+            strcpy(B[i], "Q");
+        else if (A[i] == 13)
+            strcpy(B[i], "K");
         else
-        {
-            return 0;
-        }
-    default:
-        return 0;
+            sprintf(B[i], "%d", A[i]);
+    }
+    // 调用函数F，参数为n
+    F(n);
+
+    printf("\n总共有 %d 种解法\n", answer_count);
+    for (int i = 0; i < answer_count; i++)
+    {
+        printf("%s\n", answers[i]); // 打印所有的答案
     }
 }
 
-// 计算每个数字和运算符组合的函数
-void calculate_combinations(int *numbers, char *operators, int n)
+void game2()
 {
-    // 创建一个栈来存储数字
-    double *stack = (double *)malloc(n * sizeof(double));
-    int top = -1;
-
-    // 遍历数字和运算符的数组
-    for (int i = 0; i < n; i++)
+    // 电脑随机生成四个数字
+    srand(time(NULL)); // 设置随机数种子
+    for (int i = 0; i < 4; ++i)
     {
-        // 如果我们遇到一个数字，我们就把它压入栈中
-        if (operators[i] == '\0')
+        A[i] = rand() % 13 + 1; // 生成一个1到13的随机数并存入数组A
+    }
+    // 打印出电脑生成的四个数字
+    printf("电脑生成的四个数字是：\n");
+    for (int i = 0; i < 4; ++i)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+
+    // 提示用户输入答案
+    printf("请输入您的答案：\n");
+    char answer[50];     // 定义一个长度为50的字符串answer，用于存储用户输入的答案
+    scanf("%s", answer); // 读取用户输入的答案并存入answer
+
+    // 调用函数F，参数为n
+    F(n);
+
+
+
+
+
+    // 检查用户输入的答案是否正确
+    int correct = 0; // 定义一个整数correct，用于标记答案是否正确，初始值为0
+    for (int i = 0; i < answer_count; i++)
+    {
+        if (strcmp(answer, answers[i]) == 0)
         {
-            stack[++top] = numbers[i];
-        }
-        else
-        {
-            // 如果我们遇到一个运算符，我们就从栈中弹出两个数字，进行运算，然后把结果压入栈中
-            double b = stack[top--];
-            double a = stack[top--];
-            double result = calculate(a, b, operators[i]);
-            stack[++top] = result;
+            correct = 1; // 如果用户输入的答案在answers中，将correct设为1
+            break;
         }
     }
-
-    // 最后，栈中的数字就是表达式的结果
-    double result = stack[top--];
-
-    
-    if (result - 24 < 0.000000000000000001)
-    {
-        printf("Yes\n");
-        
-    }
+    // 根据correct的值打印结果
+    if (correct)
+        printf("恭喜，您的答案是正确的！\n");
     else
     {
-        printf("No\n");
+        printf("很遗憾，您的答案是错误的。\n");
+        printf("您想查看答案吗？（1-是，2-否）：\n");
+        int choice;           // 定义一个整数choice，用于存储用户是否想查看答案
+        scanf("%d", &choice); // 读取用户的选择并存入choice
+        if (choice == 1)
+        {
+            printf("正确答案是：\n");
+            for (int i = 0; i < answer_count; i++)
+            {
+                printf("%s\n", answers[i]); // 打印所有的答案
+            }
+        }
     }
-
-    free(stack);
 }
+
 int main()
 {
-    // 输入1到13的4个整数
-    int numbers[4];
-    for (int i = 0; i < 4; i++)
+    int mode;        // 定义一个整数mode，用于存储用户选择的游戏模式
+    char play_again; // 定义一个字符play_again，用于存储用户是否想再玩一次
+
+    do
     {
-        scanf("%d", &numbers[i]);
-    }
+        // 打印欢迎信息
+        printf("#**************************************#\n");
+        printf("#                                      #\n");
+        printf("#              欢迎来到                #\n");
+        printf("#                                      #\n");
+        printf("#             24点小游戏               #\n");
+        printf("#                                      #\n");
+        printf("#**************************************#\n");
 
-    // 生成所有可能的运算符组合
-    char operator_combinations[4];
-    generate_operator_combinations(operator_combinations, 0, 4);
+        // 提示用户选择游戏模式
+        printf("1. 输入四个数字，电脑给出解法\n");
+        printf("2. 电脑给出四个数字，你自己解答\n");
+        scanf("%d", &mode); // 读取用户输入的游戏模式并存入mode
 
-    // 生成所有可能的数字组合
-    int number_combinations[4];
-    generate_number_combinations(numbers, number_combinations, 0, 4);
+        if (mode == 1)
+        {
+            game1();
+        }
+        else if (mode == 2)
+        {
+            game2();
+        }
 
-    // 计算每个数字和运算符组合的结果，并检查是否等于24
-    calculate_combinations(numbers, operator_combinations, 4);
+        // 提示用户是否想再玩一次
+        printf("你想再玩一次吗？（y/n）：\n");
+        scanf(" %c", &play_again);                    // 读取用户的选择并存入play_again
+    } while (play_again == 'y' || play_again == 'Y'); // 如果用户选择'y'或'Y'，则继续循环
 
-    return 0;
+    return 0; // 主函数返回0
 }
-
-/*
-总结并解释题目所考察的知识点：
-这个问题主要考察了如何使用递归来生成所有可能的组合，以及如何使用栈来计算表达式的值。
-
-//!关键问题1：如何生成所有可能的运算符组合？
-解释：我们可以使用递归来生成所有可能的运算符组合。首先，我们创建一个数组来存储运算符，然后我们使用递归函数来添加每个可能的运算符到数组中，直到数组的长度等于数字的数量减一。
-
-//!关键问题2：如何生成所有可能的数字组合？
-解释：我们可以使用递归来生成所有可能的数字组合。首先，我们创建一个数组来存储数字，然后我们使用递归函数来添加每个可能的数字到数组中，直到数组的长度等于数字的数量。
-
-//!关键问题3：如何计算每个数字和运算符组合的结果？
-解释：我们可以使用栈来计算每个数字和运算符组合的结果。首先，我们创建一个栈来存储数字和运算符，然后我们遍历数字和运算符的数组，如果我们遇到一个数字，我们就把它压入栈中，如果我们遇到一个运算符，我们就从栈中弹出两个数字，进行运算，然后把结果压入栈中，最后，栈中的数字就是表达式的结果。
-*/
